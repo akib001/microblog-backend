f5b53574744af970b22db08c0e34e63e8828eb21
Akib <akibahmed2011@gmail.com>
2022-06-26 18:49:11 +0600

    1st commit

---
+++ models/post.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    comments: {
+      type: Array,
+      // default: undefined
+    },
+    upVote: {
+      type: Number,
+      default: 0,
+    },
+
+    downVote: {
+      type: Number,
+      default: 0,
+    },
+    votedUser: {
+      type: Array,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', postSchema);
